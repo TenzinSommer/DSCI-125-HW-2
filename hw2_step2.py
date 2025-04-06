c13fb5e0712d9f3795b505dc06ee9c535d332939
elliotgedwards <elliotgedwards@gmail.com>
2025-04-06 13:11:41 -0400

    x

---
+++ hw2_step2.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+from nltk.corpus import stopwords
+from gensim.corpora import Dictionary
+from gensim.models import TfidfModel
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
+
+# Data Modeling 
+# pick two topics to compare
+# develop a way to identify which topic a post is talking about 
+# check occurences of different topics of conversation over time
+#   could these different conversation topics appear during a game period more often?
+# 
+
+def text2tokens(text):
+	stop_words = set(stopwords.words('english'))
+	ps = PorterStemmer()
+	text = text.lower()
+	textList = word_tokenize(text)
+	textList = [word for word in textList if word not in stopwords and len(word) >= 3]
+
+	textList = [ps.stem(word) for word in textList]
+	return textList
+    
+
+def gen_bow(df):
+	df['tokens'] = df['review'].apply(text2tokens)
+	dct = Dictionary(df['tokens'])
+	dct.filter_extremes(no_below=5, no_above=0.5)
+	df['bow'] = df['tokens'].apply(dct.doc2bow)
+	worddict = {}
+
+	for i in range(len(df)):
+		for token in df.at[i,'bow']:
+			if token in list(worddict.keys()):
+				worddict[token] += 1
+			else: 
+				worddict[token] = 1
+
+	df.drop('tokens', axis=1, inplace=True)	
+	return df, worddict
+
+print(text2tokens("In this offering, one only has to view the current Westminster Kennel Club's annual offering to see the parallels."))
+
+# df = pd.read_csv('my.csv')
+# df, dct = gen_bow(df)
+# # df.to_csv('q1_answer.csv', index=False)
+# print(df)
+
+# def get_term_doc_prob(df, dct, token):
+#     tfidf_model = TfidfModel(df['bow'].to_list())
+    
